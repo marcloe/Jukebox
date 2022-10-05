b238d9556453fceb7d43482b2562b7a0cb7bc6ed
marcloe <76460507+marcloe@users.noreply.github.com>
2022-10-04 21:32:12 -0400

    commenting

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ allButtons.forEach(button => {
 allButtons.forEach(button => {
     button.addEventListener("click", function(e) {
         let audioElement = this.querySelector("audio");
+        //State management
         if (e.target.dataset.chosen == "true") {
             e.target.classList.remove("chosen");
             e.target.dataset.chosen = false;
+            //toggle animation
             e.target.style.animationPlayState = 'paused';
+            //toggle audio
             audioElement.pause();
         } else if (e.target.dataset.chosen == "false") {
             e.target.classList.add("chosen");
