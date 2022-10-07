ebd49e8d06bb5c76d27169122f7ae780e7001203
marcloe <76460507+marcloe@users.noreply.github.com>
2022-10-06 22:04:06 -0400

    fixes with audio mechanism

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-let switchAmbient = false;
+//Declarations and Inis
 
+let switchResult = false;
+let ambientAudio = document.getElementsByClassName("result")[0].querySelector("audio");
+let allButtons = document.querySelectorAll("button");
+let audioElements = new Array();
+
+console.log(allButtons);
+console.log("audioElements")
 
 // Selector-Disselector-Mechanism
 
-allButtons = document.querySelectorAll("button");
-allButtons.forEach(button => {
-    button.style.animationPlayState = "paused";
+allButtons.forEach(elem => {
+    elem.style.animationPlayState = "paused";
 });
 
-allButtons.forEach(button => {
-    button.addEventListener("click", function(e) {
-        let audioElement = this.querySelector("audio");
+for (let i=0; i < allButtons.length; i++) { 
+    audioElements[i] = allButtons[i].querySelector("audio");
+
+    allButtons[i].addEventListener("click", function(e) {
         //State management
         if (e.target.dataset.chosen == "true") {
             e.target.classList.remove("chosen");
@@ -18,26 +25,42 @@ allButtons.forEach(button => {
             //toggle animation
             e.target.style.animationPlayState = 'paused';
             //toggle audio
-            audioElement.pause();
+            audioElements[i].pause();
+
         } else if (e.target.dataset.chosen == "false") {
             e.target.classList.add("chosen");
             e.target.dataset.chosen = true;
             //toggle animation!
             e.target.style.animationPlayState = 'running';
             //toggle audio
-            audioElement.play();
+            audioElements[i].play();
         };
     });
-});
+};
 
+// State management Result Page
 
-document.getElementById("generateButton").addEventListener("click", function() {
+document.getElementById("generateButton").addEventListener("click", function(e) {
     console.log("check");
-    if (switchAmbient==true) {
-        console.log("check");
-        document.getElementById("ambientPlayer").getElementsByTagName("source").src="ambient/ambient2.mp3"
-    };
-    switchAmbient = true;
+    if(switchResult==false) {
+        e.target.innerHTML = "Back";
+        document.getElementsByClassName("result")[0].style.visibility = "visible";
+        switchResult = true;
+        for(let i=0; i<audioElements.length; i++) {
+            audioElements[i].pause();
+        }
+        for(let r=0; r<allButtons.length; r++) {
+            allButtons[r].classList.remove("chosen");
+            allButtons[r].dataset.chosen = false;
+            //toggle animation
+            allButtons[r].style.animationPlayState = 'paused';
+        }
+        ambientAudio.play();
+    } else {
+        e.target.innerHTML = "Generate";
+        document.getElementsByClassName("result")[0].style.visibility = "hidden";
+        switchResult = false;
+        ambientAudio.pause()
+    }
 });
-
 
